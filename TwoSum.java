a3ccbf066e7b4892a5a4c73b1ae5719bfdba2b16
parker <54675124+Lourdez@users.noreply.github.com>
2024-09-22 00:41:26 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -4,7 +4,7 @@ import java.util.Map;
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> m = new HashMap<>();
-        for (int i = 0;; ++i) {
+        for (int i = 0; I <=nums.length; ++i) {
             int x = nums[i];
             int y = target - x;
             if (m.containsKey(y)) {
